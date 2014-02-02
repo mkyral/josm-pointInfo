49d5834d11b9fd6a364050cfec0aca79bfb0b8e4
Marián Kyral <mkyral@email.cz>
2014-02-02 09:16:37 +0100

    PointInfo - initial commit

---
+++ servers/RUIAN/index.php
@@ -0,0 +1,104 @@
+<?php
+require("config.php");
+$lat=$_REQUEST['lat'];
+$lon=$_REQUEST['lon'];
+if ( !is_numeric($lat) or !is_numeric($lon) ) die;
+header('Content-Type: application/json');
+
+
+$data = array();
+
+$data["coordinates"] = array( "lat" => "$lat", "lon" => "$lon");
+$data["source"] = "cuzk:ruian";
+
+
+// budova
+$query="
+  select s.kod,
+        CASE
+          WHEN s.typ_kod = 1 THEN 'Číslo popisné'
+          WHEN s.typ_kod = 2 THEN 'Číslo evidenční'
+          WHEN s.typ_kod = 3 THEN 'bez č.p./č.e.'
+          ELSE ''
+        END cislo_typ,
+        trim(both '{}' from s.cisla_domovni::text) cisla_domovni,
+        b.cislo_orientacni_hodnota || coalesce(b.cislo_orientacni_pismeno, '') cislo_orientacni,
+        s.pocet_podlazi, a.nazev, s.plati_od,
+        b.adrp_psc psc, ul.nazev ulice, c.nazev cast_obce,
+        ob.nazev obec, ok.nazev okres, vu.nazev kraj
+  from rn_stavebni_objekt s
+      left outer join osmtables.zpusob_vyuziti_objektu a on s.zpusob_vyuziti_kod = a.kod
+      left outer join rn_adresni_misto b on b.stavobj_kod = s.kod
+      left outer join rn_ulice ul on b.ulice_kod = ul.kod
+      left outer join rn_cast_obce c on c.kod = s.cobce_kod
+      left outer join rn_obec ob on ul.obec_kod = ob.kod
+      left outer join rn_okres ok on ob.okres_kod = ok.kod
+      left outer join rn_vusc vu on ok.vusc_kod = vu.kod
+  where st_contains(s.hranice,st_transform(st_geomfromtext('POINT(".$lon." ".$lat.")',4326),900913))
+  and not s.deleted
+  limit 1;
+";
+$result=pg_query($CONNECT,$query);
+
+if (pg_num_rows($result) > 0)
+{
+  $row = pg_fetch_array($result, 0);
+
+
+  $data["stavebni_objekt"] =
+    array( "ruian_id" => $row["kod"],
+           "cislo_domovni" => $row["cisla_domovni"],
+           "cislo_domovni_typ" => $row["cislo_typ"],
+           "cislo_orientacni" => $row["cislo_orientacni"],
+           "ulice" => $row["ulice"],
+           "cast_obce" => $row["cast_obce"],
+           "obec" => $row["obec"],
+           "okres" => $row["okres"],
+           "kraj" => $row["kraj"],
+           "psc" => $row["psc"],
+           "zpusob_vyuziti" => $row["nazev"],
+           "plati_od" => $row["plati_od"]);
+} else
+    $data["stavebni_objekt"] = array();
+
+// pozemek
+$query="
+  select s.id, a.nazev as druh_pozemku, b.nazev as zpusob_vyuziti, s.plati_od,
+         ku.nazev katastralni_uzemi,
+         ob.nazev obec, ok.nazev okres, vu.nazev kraj
+  from rn_parcela s
+      left outer join osmtables.druh_pozemku a on s.druh_pozemku_kod = a.kod
+      left outer join osmtables.zpusob_vyuziti_pozemku b on s.zpusob_vyu_poz_kod = b.kod
+      left outer join rn_katastralni_uzemi ku on s.katuz_kod = ku.kod
+      left outer join rn_obec ob on ku.obec_kod = ob.kod
+      left outer join rn_okres ok on ob.okres_kod = ok.kod
+      left outer join rn_vusc vu on ok.vusc_kod = vu.kod
+  where st_contains(s.hranice,st_transform(st_geomfromtext('POINT(".$lon." ".$lat.")',4326),900913))
+  and not s.deleted
+  limit 1;
+";
+
+$result=pg_query($CONNECT,$query);
+$error= pg_last_error($CONNECT);
+if (pg_num_rows($result) > 0)
+{
+  $row = pg_fetch_array($result, 0);
+
+  $data["parcela"] =
+    array( "ruian_id" => $row["id"],
+           "druh_pozemku" => $row["druh_pozemku"],
+           "zpusob_vyuziti" => $row["zpusob_vyuziti"],
+           "plati_od" => $row["plati_od"],
+           "katastralni_uzemi" => $row["katastralni_uzemi"],
+           "obec" => $row["obec"],
+           "okres" => $row["okres"],
+           "kraj" => $row["kraj"]);
+} else
+{
+//   echo "error: $error\n";
+  $data["parcela"] = array();
+}
+
+echo json_encode($data);
+
+?>
---
+++ src/org/openstreetmap/josm/plugins/pointinfo/ruianModule.java
@@ -0,0 +1,354 @@
+/**
+ *  PointInfo - plugin for JOSM
+ *  Marian Kyral
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.openstreetmap.josm.plugins.pointinfo;
+
+import org.openstreetmap.josm.data.coor.LatLon;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.util.Locale;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+class ruianRecord {
+
+    private double m_coor_lat, m_coor_lon;
+    private String m_source;
+    private long   m_objekt_ruian_id;
+    private String m_objekt_cislo_domovni;
+    private String m_objekt_cislo_domovni_typ;
+    private String m_objekt_cislo_orientacni;
+    private int    m_objekt_podlazi;
+    private String m_objekt_zpusob_vyuziti;
+    private String m_objekt_plati_od;
+    private String m_objekt_ulice;
+    private String m_objekt_cast_obce;
+    private String m_objekt_obec;
+    private String m_objekt_okres;
+    private String m_objekt_kraj;
+    private String m_objekt_psc;
+    private long   m_parcela_ruian_id;
+    private String m_parcela_druh_pozemku;
+    private String m_parcela_zpusob_vyuziti;
+    private String m_parcela_plati_od;
+    private String m_parcela_katastralni_uzemi;
+    private String m_parcela_obec;
+    private String m_parcela_okres;
+    private String m_parcela_kraj;
+
+    public ruianRecord () {
+      init();
+    }
+
+    private void init () {
+
+      m_coor_lat = 0;
+      m_coor_lon = 0;
+      m_source = "";
+      m_objekt_ruian_id = 0;
+      m_objekt_cislo_domovni = "";
+      m_objekt_cislo_domovni_typ = "";
+      m_objekt_cislo_orientacni = "";
+      m_objekt_podlazi = 0;
+      m_objekt_zpusob_vyuziti = "";
+      m_objekt_plati_od = "";
+      m_objekt_ulice = "";
+      m_objekt_cast_obce = "";
+      m_objekt_obec = "";
+      m_objekt_okres = "";
+      m_objekt_kraj = "";
+      m_objekt_psc = "";
+      m_parcela_ruian_id = 0;
+      m_parcela_druh_pozemku = "";
+      m_parcela_zpusob_vyuziti = "";
+      m_parcela_plati_od = "";
+      m_parcela_katastralni_uzemi = "";
+      m_parcela_obec = "";
+      m_parcela_okres = "";
+      m_parcela_kraj = "";
+
+    }
+
+    public void parseJSON (String jsonStr) {
+
+
+      init();
+
+
+    try {
+      JSONObject obj = new JSONObject(jsonStr);
+
+      try {
+        m_coor_lat = obj.getJSONObject("coordinates").getDouble("lat");
+      } catch (Exception e) {
+      }
+
+      try {
+        m_coor_lon = obj.getJSONObject("coordinates").getDouble("lon");
+      } catch (Exception e) {
+      }
+
+      try {
+        m_source = obj.getString("source");
+      } catch (Exception e) {
+      }
+
+
+      try {
+        JSONObject stavebniObjekt = obj.getJSONObject("stavebni_objekt");
+
+        try {
+          m_objekt_ruian_id = stavebniObjekt.getLong("ruian_id");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_cislo_domovni = stavebniObjekt.getString("cislo_domovni");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_cislo_domovni_typ = stavebniObjekt.getString("cislo_domovni_typ");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_cislo_orientacni = stavebniObjekt.getString("cislo_orientacni");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_ulice = stavebniObjekt.getString("ulice");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_cast_obce = stavebniObjekt.getString("cast_obce");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_obec = stavebniObjekt.getString("obec");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_okres = stavebniObjekt.getString("okres");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_kraj = stavebniObjekt.getString("kraj");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_psc = stavebniObjekt.getString("psc");
+        } catch (Exception e) {
+        }
+
+
+        try {
+          m_objekt_podlazi = stavebniObjekt.getInt("pocet_podlazi");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_zpusob_vyuziti = stavebniObjekt.getString("zpusob_vyuziti");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_objekt_plati_od = stavebniObjekt.getString("plati_od");
+        } catch (Exception e) {
+        }
+      } catch (Exception e) {
+      }
+
+      try {
+        JSONObject parcela = obj.getJSONObject("parcela");
+
+        try {
+          m_parcela_ruian_id = parcela.getLong("ruian_id");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_parcela_druh_pozemku = parcela.getString("druh_pozemku");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_parcela_zpusob_vyuziti = parcela.getString("zpusob_vyuziti");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_parcela_plati_od = parcela.getString("plati_od");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_parcela_katastralni_uzemi = parcela.getString("katastralni_uzemi");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_parcela_obec = parcela.getString("obec");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_parcela_okres = parcela.getString("okres");
+        } catch (Exception e) {
+        }
+
+        try {
+          m_parcela_kraj = parcela.getString("kraj");
+        } catch (Exception e) {
+        }
+
+      } catch (Exception e) {
+      }
+
+    } catch (Exception e) {
+    }
+
+    }
+
+    public String getTextCoordinates () {
+
+      String r = "";
+      DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+      symbols.setDecimalSeparator('.');
+      symbols.setGroupingSeparator(' ');
+      DecimalFormat df = new DecimalFormat("#.00000", symbols);
+
+      r = "(" + df.format(m_coor_lat) + ", " +
+                df.format(m_coor_lon) + ")";
+      return r;
+    }
+
+    public String getHtml () {
+
+      String r = "";
+
+      if (m_objekt_ruian_id == 0 && m_parcela_ruian_id == 0)
+        return r;
+
+      r = "<html>";
+      r += "<br/>";
+      if (m_objekt_ruian_id > 0) {
+        r += "<i><u>Informace o objektu</u></i><br/>";
+        r += "<b>RUIAN id: </b><a href=http://vdp.cuzk.cz/vdp/ruian/stavebniobjekty/" + m_objekt_ruian_id +">" + m_objekt_ruian_id + "</a><br/>";
+        r += "<b>Počet podlaží: </b>" + m_objekt_podlazi + "<br/>";
+        r += "<b>Způsob využití: </b>" + m_objekt_zpusob_vyuziti + "<br/>";
+        r += "<b>Platí od: </b>" + m_objekt_plati_od + "<br/>";
+        r += "<br/>";
+        if (m_objekt_cislo_domovni == null || m_objekt_cislo_domovni.isEmpty()) {
+          r += "<b>Budova: </b>" + m_objekt_cislo_domovni_typ + "<br/>";
+          r += "<b>Obec: </b>" + m_parcela_obec +"<br/>";
+          r += "<b>Okres: </b>" + m_parcela_okres +"<br/>";
+          r += "<b>Kraj: </b>" + m_parcela_kraj +"<br/>";
+        } else {
+          String x = "";
+          if ( !m_objekt_cislo_orientacni.isEmpty()) {
+            x = "/" + m_objekt_cislo_orientacni;
+          }
+          r += "<b>" + m_objekt_cislo_domovni_typ + ": </b>" + m_objekt_cislo_domovni + x + "<br/>";
+          r += "<b>Ulice: </b>" + m_objekt_ulice + "<br/>";
+          r += "<b>Část obce: </b>" + m_objekt_cast_obce + "<br/>";
+          r += "<b>Obec: </b>" + m_objekt_obec + "<br/>";
+          r += "<b>Okres: </b>" + m_objekt_okres + "<br/>";
+          r += "<b>Kraj: </b>" + m_objekt_kraj + "<br/>";
+          r += "<b>PSČ: </b>" + m_objekt_psc + "<br/>";
+        }
+        r += "<br/>";
+      }
+      if (m_parcela_ruian_id > 0) {
+        r += "<i><u>Informace o pozemku</u></i><br/>";
+        r += "<b>RUIAN id: </b><a href=http://vdp.cuzk.cz/vdp/ruian/parcely/" + m_parcela_ruian_id +">" + m_parcela_ruian_id + "</a><br/>";
+        r += "<b>Druh pozemku: </b>" + m_parcela_druh_pozemku +"<br/>";
+        r += "<b>Způsob využití: </b>" + m_parcela_zpusob_vyuziti +"<br/>";
+        r += "<b>Platí od: </b>" + m_parcela_plati_od +"<br/>";
+        r += "<br/>";
+        r += "<b>Katastrální území: </b>" + m_parcela_katastralni_uzemi +"<br/>";
+        if ( m_objekt_ruian_id == 0) {
+          r += "<b>Obec: </b>" + m_parcela_obec +"<br/>";
+          r += "<b>Okres: </b>" + m_parcela_okres +"<br/>";
+          r += "<b>Kraj: </b>" + m_parcela_kraj +"<br/>";
+        }
+      }
+      r += "<hr/>";
+      r += "<center><i><small>Zdroj: <a href=\"http://www.ruian.cz/\">" + m_source + "</a></small></i></center>";
+      r += "</html>";
+
+      return r;
+    }
+}
+
+public class ruianModule {
+
+    private String m_text = "";
+    private String URL = "http://poloha.net/~marian/index.php";
+    protected PointInfoServer server = new PointInfoServer();
+
+    private ruianRecord m_record = new ruianRecord();
+
+    public ruianModule() {
+
+    }
+
+    /**
+     * Return Html text representation
+     * @return String htmlText
+     */
+    public String getHtml() {
+
+      return m_record.getHtml();
+    }
+
+    /**
+     * Return coordinates text representation
+     * @return String coordinatesText
+     */
+    public String getTextCoordinates() {
+
+      return m_record.getTextCoordinates();
+    }
+
+    /**
+     * Trace building on position.
+     * @param pos Position on the map
+     */
+    public void prepareData(LatLon pos) {
+        try {
+
+             String request = URL + "?lat=" + pos.lat() + "&lon=" + pos.lon();
+             System.out.println("Request: "+ request);
+             String content = server.callServer(request);
+             System.out.println("Reply: " + content);
+             m_record.parseJSON(content);
+        } catch (Exception e) {
+
+        }
+    }
+}
